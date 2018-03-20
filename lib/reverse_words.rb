23570d8d212ba5f819e8cd62f87753459de0b4a0
Phoebe <phoebe.manning8@gmail.com>
2018-03-20 11:56:53 -0700

    completed method

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,4 +1,33 @@
 # A method to reverse each word in a sentence, in place.
 def reverse_words(my_words)
-  raise NotImplementedError
+  if my_words == nil
+    return nil
+  elsif my_words.length == 1
+    return my_words
+  end
+
+  i = 0
+  j = 0
+  k = 0
+  while my_words[k] != nil
+
+    while my_words[k] != " " && my_words[k] != nil
+      k += 1
+    end
+
+    k > 0 ? j = k - 1 : j = k
+    while i < j
+      temp = my_words[i]
+      my_words[i] = my_words[j]
+      my_words[j] = temp
+
+      i += 1
+      j -= 1
+    end
+
+    k += 1
+    i = k
+  end
+
+  return my_words
 end
--- specs/reverse_words_spec.rb
+++ specs/reverse_words_spec.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 require_relative '../lib/reverse_words'
 
 describe "reverse words" do
